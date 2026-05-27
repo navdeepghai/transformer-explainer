3c880514cc23da3098802f4ee162b38a7e52645c
Navdeep Singh <navdeepghai1@gmail.com>
2026-05-27 08:31:49 -0400

    feat: fixes for vite version mismatch and vite config for missing sass files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const variablesPath = path
+	.resolve(__dirname, 'src/styles/variables.scss')
+	.replace(/\\/g, '/');
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData: `@import 'src/styles/variables.scss';`
+				additionalData: `@import '${variablesPath}';`
 			}
 		}
 	},
